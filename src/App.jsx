81ad9af7a8ec9e0fedd98d92bbecd0b11b4c18a0
Waqas Khan <waqaskhan777id@gmail.com>
2023-02-18 01:46:10 +0500

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 // import { useState } from 'react'
 // import Titles from './components/Titles'
-import MostRecentEQ from './FetchingData/MostRecentEQ'
+// import MostRecentEQ from './FetchingData/MostRecentEQ'
+import useFetch from './FetchingData/useFetch'
+
 
 function App() {
 
-  function fetchMostRecentEQ(){
-    return <MostRecentEQ />
-  }
+  const [data, isPending, error] = useFetch('https://everyearthquake.p.rapidapi.com/earthquakes?start=1&count=2&type=earthquake&latitude=33.962523&longitude=-118.3706975&radius=1000&units=miles&magnitude=3&intensity=1', '26817802b9msh3b85000919b2cc8p11516ajsn916953542d0e', 'everyearthquake.p.rapidapi.com')
+
+
+  // function fetchMostRecentEQ(){
+  //   return <MostRecentEQ />
+  // }
+
 
   
   return (
     <div className="App">
+      {      
+        error && <div>{error}</div>
+      }
       {
-
+        isPending && <div>Loading...</div>
+      }
+      {
+        data && <div>{data}</div>
       }
       {/* <Titles /> */}
       <button onClick={fetchMostRecentEQ}>Fetch</button>
--- src/FetchingData/useFetch.jsx
+++ src/FetchingData/useFetch.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react'
 
-
+// Custom hook created to fetch data from the API
 const useFetch = (url,key, host) => {
     const [data, setdata]= useState(null)
     const [isPending, setisPending]= useState(true)
@@ -15,11 +15,8 @@ const useFetch = (url,key, host) => {
           'X-RapidAPI-Key': key,
           'X-RapidAPI-Host': host
         }
-      };
-      
-//       fetch('https://everyearthquake.p.rapidapi.com/earthquakes?start=1&count=2&type=earthquake&latitude=33.962523&longitude=-118.3706975&radius=1000&units=miles&magnitude=3&intensity=1', options)
-//         
-
+      }  
+    
         fetch(url)
         .then(res=>{
             if(!res.ok){
@@ -38,37 +35,7 @@ const useFetch = (url,key, host) => {
         })
 
     },[url])
-    return {data, isPending, error}
-
-
-    
-    
+    return {data, isPending, error}    
 }
 export default useFetch;
 
-// function useFetch(url){
-//     const [data,setdata]= useState(null)
-//     const [isPending,setisPending]=useState(true)
-//     const [error, seterror]=useState(null)
-
-//     useEffect(()=>{
-//         fetch(url)
-//         .then(res=>{
-//             if(!res.ok){
-//                 throw Error ('Could not fetch the data')
-//             }
-//             return res.json
-//         })
-//         .then(data=>{
-//             setdata(data)
-//             setisPending(false)
-//             seterror(null)
-//         })
-//         .catch(err=>{
-//             isPending(false)
-//             seterror(err.message)
-//         })
-//     },[url])
-//     return {data, isPending, error}
-
-// }
